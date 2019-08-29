146ae2487868ec1712ac901ad223b407b23ab2af
Simone Pitzianti <simone.pitzianti@sardex.net>
2019-08-29 14:28:36 +0200

    Fix touchId interface in a safer way

--- www/touchid.js
+++ www/touchid.js
@@ -1,29 +1,32 @@
 var argscheck = require('cordova/argscheck'),
-               exec = require('cordova/exec');
+	exec = require('cordova/exec');
 
 var touchid = {
-	isAvailable: function(successCallback, errorCallback){
-		exec(successCallback, errorCallback, "TouchID", "isAvailable", []);
+	isAvailable: function(successCallback, errorCallback) {
+		exec(successCallback, errorCallback, 'TouchID', 'isAvailable', []);
 	},
-	save: function(key,password, userAuthenticationRequired, successCallback, errorCallback) {
-		exec(successCallback, errorCallback, "TouchID", "save", [key,password, userAuthenticationRequired]);
+	save: function(key, password, successCallback, errorCallback) {
+		exec(successCallback, errorCallback, 'TouchID', 'save', [
+			key,
+			password,
+			true,
+		]);
 	},
-	verify: function(key,message,successCallback, errorCallback){
-		exec(successCallback, errorCallback, "TouchID", "verify", [key,message]);
+	verify: function(key, message, successCallback, errorCallback) {
+		exec(successCallback, errorCallback, 'TouchID', 'verify', [key, message]);
 	},
-	has: function(key,successCallback, errorCallback){
-		exec(successCallback, errorCallback, "TouchID", "has", [key]);
+	has: function(key, successCallback, errorCallback) {
+		exec(successCallback, errorCallback, 'TouchID', 'has', [key]);
 	},
-	delete: function(key,successCallback, errorCallback){
-		exec(successCallback, errorCallback, "TouchID", "delete", [key]);
+	delete: function(key, successCallback, errorCallback) {
+		exec(successCallback, errorCallback, 'TouchID', 'delete', [key]);
 	},
-	setLocale: function(locale,successCallback, errorCallback){
-		exec(successCallback, errorCallback, "TouchID", "setLocale", [locale]);
+	setLocale: function(locale, successCallback, errorCallback) {
+		exec(successCallback, errorCallback, 'TouchID', 'setLocale', [locale]);
 	},
-	move: function(key, packageName,successCallback, errorCallback){
-    	exec(successCallback, errorCallback, "TouchID", "move", [key,packageName]);
-    }
-
+	move: function(key, packageName, successCallback, errorCallback) {
+		exec(successCallback, errorCallback, 'TouchID', 'move', [key, packageName]);
+	},
 };
 
 module.exports = touchid;
